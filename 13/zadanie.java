d03939b6dd034c940df828d13c2ff67fe022ae81
Vesse00 <vessepl@gmail.com>
2021-02-23 09:21:29 +0100

    Update zadanie.java

--- 13/zadanie.java
+++ 13/zadanie.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class zadanie{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int[] los = new int[10];
+
+        Random rand = new Random();
+        for(int i=0; i<los.length; i++){
+            los[i] = (int)Math.random()*6+1;
+        }
 
     }
 }
-    
