91962203e8aeb0c3d0544b6a2febf43eaffbcf40
Vesse00 <vessepl@gmail.com>
2021-02-23 09:51:35 +0100

    y

--- 13/zadanie.java
+++ 13/zadanie.java
@@ -1,16 +1,34 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.io.*;
 
-class zadanie{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
+class zadanie {
+    public static void main(String[] args) {
+
         int[] los = new int[10];
 
-        Random rand = new Random();
-        for(int i=0; i<los.length; i++){
-            los[i] = (int)Math.random()*6+1;
+        for (int i = 0; i < los.length; i++) {
+            Random rand = new Random();
+            los[i] = (int) (Math.random() * 6 + 1);
         }
+
+        /*
+         * for(int i=0; i<los.length; i++){ System.out.println("-> "+los[i]); }
+         */
+
+        //File plik = new File("uczniowie.txt");
+        try {
+            Scanner odczyt = new Scanner(new File("uczniowie.txt"));
+            while(odczyt.hasNext()){
+                String text = (odczyt.nextLine());
+                System.out.println(text);
+            }
+            
+        } catch (FileNotFoundException e) {
+            
+            e.printStackTrace();
+        }
+
 
     }
 }
