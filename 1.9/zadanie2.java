49566a3cd50c8ddedfcde0d8554d18ffcdfa8c6c
Vesse00 <vessepl@gmail.com>
2021-02-24 10:23:06 +0100

    Update zadanie2.java

--- 1.9/zadanie2.java
+++ 1.9/zadanie2.java
@@ -13,25 +13,25 @@ public class zadanie2 {
                 czylos[i++] = sc.nextInt();
             }
 
+            Scanner sc2 = new Scanner(new File("ileliczb.txt"));
+            int [] ile = new int[8];
+            int j = 0;
+            while(sc2.hasNextInt()){
+                ile[j++] = sc2.nextInt();
+            }
+
             //for(int j = 0; j < czylos.length; j++){ System.out.println(czylos[j]);}
 
-            for(int j = 0; j < czylos.length; j++){
+            for(j = 0; j < czylos.length; j++){
                 if(czylos[j] == 0){
                     System.out.println("Wczytalem 0 - nie losuje");
                 }else if(czylos[j] == 1){
-
-                    Scanner sc2 = new Scanner(new File("ileliczb.txt"));
-                    int [] ile = new int[8];
-                    int a = 0;
-                    while(sc2.hasNextInt()){
-                        ile[j++] = sc2.nextInt();
-                    }
 
                     for(j = 0; j < ile.length; j++){ 
                         System.out.println("Czy los: "+czylos[j]+" Ile los: "+ile[j]);
                     }
 
-
+                    
                     
                 }
             }
