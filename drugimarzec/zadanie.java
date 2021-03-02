ff880dfe0a366ffa9b59a8e025e5de13b5c25456
Vesse00 <vessepl@gmail.com>
2021-03-02 09:59:06 +0100

    akual zadania

---
+++ drugimarzec/zadanie.java
@@ -0,0 +1,18 @@
+package drugimarzec;
+
+public class zadanie {
+    public static void main(String[] args) {
+        int [][] tab = new int[2][2];
+
+        tab[1][1] = 55;
+        tab[1][0] = 6;
+        tab[0][1] = 99;
+        tab[0][0] = 60;
+
+        for(int i = 0; i < tab.length; i++){
+            for(int j = 0; j < tab.length; j++){
+                System.out.println(tab[i][j]);
+            }
+        }
+    }
+}
