2aa05c90e38c0067e86126783506bd363850a3a0
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2021-03-09 10:16:38 +0100

    Rekurencja

---
+++ lekcja/Marzec/2tydzien/Rekurencja2.java
@@ -0,0 +1,22 @@
+class Rekurencja2{
+    public static void main(String[] args){
+        int liczba = sumaKolejnychLiczb(3);
+        System.out.println("Wynik: "+liczba);
+
+        int tak = obliczanieSilni(15);
+        System.out.println("Wynik: "+tak);
+        
+    }
+
+    private static int sumaKolejnychLiczb(int liczba){
+        if(liczba>0) return liczba+sumaKolejnychLiczb(liczba-1);
+        return 0;
+    }
+
+    private static int obliczanieSilni(int liczba){
+        if(liczba > 1){
+            return liczba * obliczanieSilni(liczba-1);
+        }
+        return 1;
+    }
+}
