febf72f75eee8f7779728dd6ba689c9f78fad95c
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2021-03-16 10:04:57 +0100

    PolaPrywatnePubliczne
    commit man

---
+++ PolaPrywatnePubliczne/DaneOsobowe.java
@@ -0,0 +1,6 @@
+public class DaneOsobowe {
+   private String pesel = "12345678901";
+   public int wiek = 20;
+   public int wzrost = 188;
+   public int waga = 88;
+}
---
+++ PolaPrywatnePubliczne/PolaPrywatnePubliczne.java
@@ -0,0 +1,10 @@
+public class PolaPrywatnePubliczne {
+
+    static DaneOsobowe dos;
+    public static void main(String[] args){
+        
+        dos = new DaneOsobowe(); //od tego momentu moge uzywac nazwy: dos
+
+        System.out.println(dos.wzrost);
+    }
+}
