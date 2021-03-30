3c91b793725263760f30beaf73c1bbb56120b7ba
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2021-03-30 08:59:54 +0200

    PUBLIC Strin g
    imie nazwisko klasa

---
+++ 1.9.1/CwiczenieObiektow_1.java
@@ -0,0 +1,5 @@
+class CwiczenieObiektow_1{
+    public static void main(String[] arg){
+        
+    }
+}
---
+++ 1.9.1/MojeDane.java
@@ -0,0 +1,8 @@
+public class MojeDane {
+    public String imie = "";
+    public String nazwisko = "";
+    public String klasa = "";
+
+
+
+}
