fcbcbe9acd175ba268d00b4f2c2e1717aa709ac1
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2021-03-30 09:12:50 +0200

    Update CwiczenieObiektow_1.java

--- 1.9.1/CwiczenieObiektow_1.java
+++ 1.9.1/CwiczenieObiektow_1.java
@@ -1,6 +1,8 @@
 class CwiczenieObiektow_1{
     public static void main(String[] arg){
-        
+        MojeDane md = new MojeDane("Maciej", "Szczypta", "2PTN");
+
+        System.out.println(md.imie+"\n "+md.nazwisko+"\n "+md.klasa);
         
     }
 }
