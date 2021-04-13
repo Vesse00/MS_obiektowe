970dcebe862fe5ef6f530dfde435b624b7ba4670
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2021-04-13 09:35:45 +0200

    Trojkat normalny

---
+++ wielekonstr/Main.java
@@ -0,0 +1,22 @@
+import jdk.vm.ci.meta.TriState;
+
+class Main{
+    public static void main(String[] args){
+
+        /*Osoba os = new Osoba(177,80);
+        System.out.println(os);
+
+        System.out.println();
+        
+        Osoba os2 = new Osoba("Maciej","Szczypta");
+        System.out.println(os2);
+
+        System.out.println();
+
+        Osoba os3 = new Osoba("Maciej","Szczypta", 180, 60);
+        System.out.println(os3);*/
+
+        Trojkat t1 = new Trojkat(10, 15);
+        System.out.println(t1);
+    }
+}
---
+++ wielekonstr/Trojkat.java
@@ -0,0 +1,15 @@
+class Trojkat {
+    /*
+    * bylo kilka konstroktowo rozne trojkaty
+    */
+
+    
+
+    //Konstruktor1 Trojkat normal
+    public Trojkat(int a,int h){
+        System.out.println("Pole trojkata: " + (a*h/2));
+    }
+
+
+    
+}
